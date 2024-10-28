599b0e90962a11753f01f9c38adeec788425eddb
axelwarn2 <722shock722@mail.ru>
2024-10-28 16:42:04 +0500

    14043: Добавит текст

---
+++ www/about_clinic/index.php
@@ -0,0 +1,18 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+?>
+<h2>О клинике</h2>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat dignissimos nisi omnis, a reiciendis obcaecati ad neque
+inventore aperiam, vitae aliquam ratione et sint incidunt odio tenetur molestias, accusantium dolore?
+Excepturi molestias nesciunt nulla molestiae neque iure nisi expedita dicta inventore est, a perferendis magnam delectus
+non qui recusandae corporis natus atque magni obcaecati. Quia aliquam laudantium recusandae eius. Voluptatibus.
+Molestiae consequuntur placeat voluptas minus recusandae fuga provident dolorum laborum odit pariatur, delectus facilis
+ad? Vitae nihil nemo dolorem iste ducimus cupiditate, sapiente laboriosam? Quam saepe sit dolores at ducimus?
+Voluptas facilis eius tempora perspiciatis optio sapiente ipsa aliquid esse quaerat expedita? Eveniet cupiditate ea
+dignissimos quisquam? Cum voluptatibus est ullam libero possimus quis neque at, explicabo iste quas aspernatur.
+Beatae illum error, eius eaque ex quas assumenda natus, animi hic et earum necessitatibus maxime eos repudiandae dolore,
+itaque veritatis at recusandae magnam totam facilis qui fugit alias labore. Aut.
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>
+
