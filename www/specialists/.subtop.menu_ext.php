ca79a0b0e2ea275d807c0dc7752cec1b84bd3dd2
Константин <165680333+axelwarn2@users.noreply.github.com>
2024-11-15 16:58:02 +0500

    14043: Добавит кэширование

--- www/specialists/.subtop.menu_ext.php
+++ www/specialists/.subtop.menu_ext.php
@@ -6,26 +6,39 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 
 use Bitrix\Main\Loader;
 use Bitrix\Iblock\Elements\ElementSpecialistsTable as Specialists;
+use Bitrix\Main\Data\Cache;
 
 Loader::includeModule('iblock');
 
 $aMenuLinksExt = [];
+$cacheTime = 3600;
+$cacheId = 'specialist_menu';
+$cacheDir = '/bitrix/cache/specialists_menu';
+$cache = Cache::createInstance();
 
-$elements = Specialists::getList([
-    'select' => ['CODE', 'NAME'],
-    'filter' => [
-        'ACTIVE' => 'Y',
-    ],
-])->fetchAll();
+if ($cache->initCache($cacheTime, $cacheId, $cacheDir)) {
+    $aMenuLinksExt = $cache->getVars();
+} elseif ($cache->startDataCache()) {
+    $elements = Specialists::getList([
+        'select' => ['CODE', 'NAME'],
+        'filter' => ['ACTIVE'=>'Y'],
+    ])->fetchAll();
 
-foreach ($elements as $element) {
-    $aMenuLinksExt[] = [
-        $element['NAME'],
-        '/specialists/' . $element['CODE'] . "/",
-        [],
-        [],
-        '',
-    ];
+    foreach ($elements as $element) {
+        $aMenuLinksExt[] = [
+            $element['NAME'],
+            '/specialists/' . $element['CODE'] . "/",
+            [],
+            [],
+            '',
+        ];
+    }
+
+    if (!empty($aMenuLinksExt)){
+        $cache->endDataCache($aMenuLinksExt);
+    } else {
+        $cache->abortDataCache();
+    }
 }
 
 $aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
